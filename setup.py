0aef9abf3791cdde33a401c23c0714801de3f8ef
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-02-23 16:59:19 -0500

    fix: rm logging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py_clob_client",
-    version="0.0.2",
+    version="0.0.3",
     author="Jonathan Amenechi",
     author_email="jonathanamenechi@gmail.com",
     description="Python client for the Polymarket CLOB",
