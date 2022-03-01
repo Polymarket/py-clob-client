8fa96760a57611d8cfdf12f176909f9b3d195a41
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-03-01 15:33:35 -0500

    fix: use signer chain id when building 712 sigs

--- py_clob_client/signing/eip712.py
+++ py_clob_client/signing/eip712.py
@@ -7,12 +7,13 @@ CLOB_DOMAIN_NAME = "ClobAuthDomain"
 CLOB_VERSION = "1"
 MSG_TO_SIGN = "This message attests that I control the given wallet"
 
-def get_clob_auth_domain():
+def get_clob_auth_domain(chain_id:int):
     return make_domain(name=CLOB_DOMAIN_NAME, version=CLOB_VERSION, chainId=1)
 
 def sign_clob_auth_message(signer: Signer, timestamp: int)-> str:
     clob_auth_msg = ClobAuth(address=signer.address, timestamp=str(timestamp), message=MSG_TO_SIGN)
-    auth_struct_hash = Web3.keccak(clob_auth_msg.signable_bytes(get_clob_auth_domain()))
+    chain_id = signer.get_chain_id()
+    auth_struct_hash = Web3.keccak(clob_auth_msg.signable_bytes(get_clob_auth_domain(chain_id)))
     return signer.sign(auth_struct_hash)
 
 
