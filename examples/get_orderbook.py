9ab07c4d738b3141349d7a2ec7b4bcae1efcd9b0
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-03-07 11:35:27 -0500

    fix: add endpoints, update get_order_book and midpoint to adhere to new standard

---
+++ examples/get_orderbook.py
@@ -0,0 +1,9 @@
+from py_clob_client.client import ClobClient
+
+def main():
+    host = "http://localhost:8080"
+    client = ClobClient(host)
+
+    print(client.get_order_book("16678291189211314787145083999015737376658799626183230671758641503291735614088"))
+
+main()
