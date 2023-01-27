78025df0cffa4c24bb3d29e4b9dd354bea968171
Rodrigo <95635797+poly-rodr@users.noreply.github.com>
2023-01-27 17:12:06 -0300

    removing extra test case

--- tests/order_builder/test_helpers.py
+++ tests/order_builder/test_helpers.py
@@ -6,5 +6,4 @@ from py_clob_client.order_builder.helpers import decimal_places
 class TestHelpers(TestCase):
     def test_decimal_places(self):
         self.assertEqual(decimal_places(949.9970999999999), 13)
-        self.assertEqual(decimal_places(949.9970999999999), 13)
         self.assertEqual(decimal_places(949), 0)
