8abcae5187832752593a30e36159a46d8d76b86e
mike <mshrieve@gmail.com>
2023-02-16 12:19:44 -0800

    Remove Logging of Headers (#52)
    * remove print headers
    
    * bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py_clob_client",
-    version="0.2.2",
+    version="0.2.3",
     author="Polymarket Engineering",
     author_email="engineering@polymarket.com",
     maintainer="Polymarket Engineering",
